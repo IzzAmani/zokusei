ccea2ab6888d09e5668469c105dec61086adb3a1
saturnSolar <saturnsolar1093@gmail.com>
2025-06-25 01:28:33 +0800

    Added main menu with animations.

--- Objs/Door/door.gd
+++ Objs/Door/door.gd
@@ -4,16 +4,16 @@ extends Area2D
 var endDoor: Marker2D
 
 func _ready():
-    if endDoorPath:
-        endDoor = get_node(endDoorPath)
-    else:
-        push_error("END DOOR PATH NOT DEFINED")
-        
+	if endDoorPath:
+		endDoor = get_node(endDoorPath)
+	else:
+		push_error("END DOOR PATH NOT DEFINED")
+		
 func _on_body_entered(body: Node2D) :
-    if body.name != "Player" :
-        return
-        
-    if endDoor:
-        body.global_position = endDoor.global_position
-        
-    
+	if body.name != "Player" :
+		return
+		
+	if endDoor:
+		body.global_position = endDoor.global_position
+		
+	
