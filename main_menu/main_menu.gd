0485b43e57b1f8c64e629693c476b32cdc507793
saturnSolar <saturnsolar1093@gmail.com>
2025-06-25 16:21:55 +0800

    Added 'click to start' function.

---
+++ main_menu/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+signal show_menu
+var clicked = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_pressed() && clicked == false:
+		print("done")
+		clicked = true
+		emit_signal("show_menu")
+		$Flash.visible = false
+		
--- main_menu/title_animation.gd
+++ main_menu/title_animation.gd
@@ -1,6 +1,12 @@
 extends AnimationPlayer
 
 
-
 func _on_animation_finished(anim_name: StringName) -> void:
-	play("floating_title")
+	if anim_name == "startup":
+		play("flash")
+		$"../TextureRect/AnimationPlayer".play("floating_title")
+		$"../Hint/AnimationPlayer".play("blinking")
+
+
+func _on_main_menu_show_menu() -> void:
+	play("showmenu")
