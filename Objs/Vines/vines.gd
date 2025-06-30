f0e62e2899c3b99eea3b88646787c7929f326475
saturnSolar <saturnsolar1093@gmail.com>
2025-06-30 18:49:45 +0800

    Added burning and explosion when burned (code is funny)

--- Objs/Vines/vines.gd
+++ Objs/Vines/vines.gd
@@ -9,11 +9,16 @@ var tablet_visible       := false
 # Dict => Attr_Name : [type(bool, int, str), whatToChange ]
 @export var attributes: Dictionary
 @onready var label_anim = $Icon/IconTexture/AnimationPlayer
+@onready var texture = $Fire/Texture
+@onready var timer = $Fire/Timer
+
 
 func _ready():
     #hint.visible = false
+    texture.visible = false
     add_to_group("Objects")
     add_to_group("Vines")
+    timer.connect("timeout", on_timeout)
     
     for attr in attributes :
         if attr == "Collision" :
@@ -59,5 +64,21 @@ func _input(evt) -> void :
             emit_signal("toggle_tablet", tablet_visible)
             
     elif evt.is_action_pressed("rightClick") and attributes["Flammable"] :
-        if mouse_entered_vines and player_entered_vines:
-            queue_free()
+        if mouse_entered_vines and player_entered_vines and !texture.visible:
+            texture.visible = true
+            $Sprite2D.modulate = Color(0.969, 0.153, 0.0, 1.0)
+            timer.start()
+
+func on_timeout() -> void:
+    timer.stop()
+    $Sprite2D.visible = false
+    $Icon.visible = false
+    texture.visible = false
+    $Explosion.emitting = true
+    $Hitbox/CollisionShape2D.disabled = true
+    $Area/CollisionShape2D.disabled = true
+    $Range/CollisionShape2D.disabled = true
+
+
+func _on_explosion_finished() -> void:
+    queue_free()
